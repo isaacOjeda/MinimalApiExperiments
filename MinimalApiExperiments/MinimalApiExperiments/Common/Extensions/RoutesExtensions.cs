97b112469d09a032d527646f54b4bd0314ec56b7
Isaac Ojeda <isaac.ojeda@intelectix.com>
2022-11-11 08:42:41 -0600

    minor

--- MinimalApiExperiments/MinimalApiExperiments/Common/Extensions/RoutesExtensions.cs
+++ MinimalApiExperiments/MinimalApiExperiments/Common/Extensions/RoutesExtensions.cs
@@ -5,39 +5,26 @@ namespace MinimalApiExperiments.Common.Extensions;
 public static class RoutesExtensions
 {
 
-    public static RouteHandlerBuilder MediatrGet<TRequest>(this RouteGroupBuilder group, string template)
-        where TRequest : IHttpRequest
-    {
-        return group.MapGet(template, (IMediator mediator, [AsParameters] TRequest request) =>
-            mediator.Send(request)
-        )
+    public static RouteHandlerBuilder MediatrGet<TRequest>(this RouteGroupBuilder group, string template) where TRequest : IHttpRequest =>
+        group.MapGet(template, (IMediator mediator, [AsParameters] TRequest request) =>
+            mediator.Send(request))
         .WithName(typeof(TRequest).Name);
-    }
 
-    public static RouteHandlerBuilder MediatrPost<TRequest>(this RouteGroupBuilder group, string template)
-        where TRequest : IHttpRequest
-    {
-        return group.MapPost(template, (IMediator mediator, [AsParameters] TRequest request) =>
-            mediator.Send(request)
-        )
+    public static RouteHandlerBuilder MediatrPost<TRequest>(this RouteGroupBuilder group, string template) where TRequest : IHttpRequest =>
+        group.MapPost(template, (IMediator mediator, [AsParameters] TRequest request) =>
+            mediator.Send(request))
         .WithName(typeof(TRequest).Name);
-    }
 
-    public static RouteHandlerBuilder MediatrPut<TRequest>(this RouteGroupBuilder group, string template)
-        where TRequest : IHttpRequest
-    {
-        return group.MapPut(template, (IMediator mediator, [AsParameters] TRequest request) =>
-            mediator.Send(request)
-        )
-        .WithName(typeof(TRequest).Name);
-    }
 
-    public static RouteHandlerBuilder MediatrDelete<TRequest>(this RouteGroupBuilder group, string template)
-        where TRequest : IHttpRequest
-    {
-        return group.MapDelete(template, (IMediator mediator, [AsParameters] TRequest request) =>
-            mediator.Send(request)
-        )
+    public static RouteHandlerBuilder MediatrPut<TRequest>(this RouteGroupBuilder group, string template) where TRequest : IHttpRequest =>
+        group.MapPut(template, (IMediator mediator, [AsParameters] TRequest request) =>
+            mediator.Send(request))
         .WithName(typeof(TRequest).Name);
-    }
+
+
+    public static RouteHandlerBuilder MediatrDelete<TRequest>(this RouteGroupBuilder group, string template) where TRequest : IHttpRequest =>
+        group.MapDelete(template, (IMediator mediator, [AsParameters] TRequest request) =>
+            mediator.Send(request))
+        .WithName(typeof(TRequest).Name);
+
 }
